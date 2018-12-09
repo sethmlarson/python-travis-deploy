bdbdf6313571012b5b9c42fdaf965be475dc453d
Seth Michael Larson <sethmichaellarson@gmail.com>
2018-12-08 20:22:40 -0600

    1.3.0

--- _travis/deploy.sh
+++ _travis/deploy.sh
@@ -6,5 +6,5 @@ set -x
 python -m pip install -U twine
 twine --version
 
-python setup.py build $PYPI_BUILD
+python setup.py build $1
 twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD --skip-existing
