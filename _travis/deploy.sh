45f985ffa5097cd92b64681fdec12ba7a84102a6
Seth Michael Larson <sethmichaellarson@gmail.com>
2018-12-08 20:37:22 -0600

    1.5.0

--- _travis/deploy.sh
+++ _travis/deploy.sh
@@ -3,8 +3,12 @@
 set -e
 set -x
 
-python -m pip install -U twine
+python -m pip install -U twine setuptools wheel
 twine --version
 
-python setup.py build $1
+python setup.py build bdist_wheel
+if [ "$TOXENV" = "py36" ]; then
+  python setup.py build sdist
+fi
+
 twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD --skip-existing
